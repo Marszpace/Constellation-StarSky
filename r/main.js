81c9e420a1836249223779ab194de1fd21b0de68
Galax <72642849+Galax028@users.noreply.github.com>
2021-03-26 17:49:51 +0700

    Refactored and made code prettier

--- r/main.js
+++ r/main.js
@@ -1,73 +1,71 @@
-//Define Global Variables
-var all
-var time
-var now
-var correct
+// Define Global Variables
+var all = [];
+var time = 0;
+var now;
+var correct;
 
-//Functions
-function startPractice(){
-  correct=0
-  for(i=1, i<=88, i++){
-    all.push(i)
+// Functions
+function startPractice() {
+  correct = 0;
+  for (var i = 1; i <= 88; i++) {
+    all.push(i);
   }
   time = document.getElementById("time").value;
-  document.getElementById("file").style.opacity="1";
-  document.getElementById("show").style.display="inline";
-  document.getElementById("ans").style.display="inline";
-  document.getElementById("answ").style.display="inline";
+  document.getElementById("file").style.opacity = "1";
+  document.getElementById("show").style.display = "inline";
+  document.getElementById("ans").style.display = "inline";
+  document.getElementById("answ").style.display = "inline";
   cont();
 }
 
-function cont(){
-  //End
-  if (time==0){
-    stopPractice();
-    return;
-    }
-  document.getElementById("cont").style.display="none";
-  document.getElementById("ans").style.display="inline";
-  //Random
+function cont() {
+  // End
+  if (time === 0) {
+    return stopPractice();
+  }
+  document.getElementById("cont").style.display = "none";
+  document.getElementById("ans").style.display = "inline";
+  // Random
   now = all[Math.floor(Math.random() * all.length)];
-  //Questions
-  document.getElementById("file").setAttribute("src","i/"+con[now].file+".gif");
+  // Questions
+  document.getElementById("file").setAttribute("src", "i/" + con[now].file + ".gif");
   document.getElementById("name");
-  document.getElementById("coor").innerHTML=con[now].coor
-  //Remove Asked Ones
+  document.getElementById("coor").innerHTML = con[now].coor;
+  // Remove Asked Ones
   var index = all.indexOf(now);
   if (index > -1) {
     all.splice(index, 1);
   }
-  //Count
-  time--
+  // Count
+  --time;
 }
 
-function showAnswer(){
-  document.getElementById("cont").style.display="inline";
-  document.getElementById("ans").style.display="none";
-  document.getElementById("name").innerHTML=con[now].name;
+function showAnswer() {
+  document.getElementById("cont").style.display = "inline";
+  document.getElementById("ans").style.display = "none";
+  document.getElementById("name").innerHTML = con[now].name;
 }
 
-function answer(){
-  document.getElementById("cont").style.display="inline";
+function answer() {
+  document.getElementById("cont").style.display = "inline";
   var answ = document.getElementById("answ").value;
-  if (answ==con[now].name){
-    correct++
-    document.getElementById("name").innerHTML="Correct!";
-  }
-  else {
-    document.getElementById("name").innerHTML="Wrong!";
+  if (answ === con[now].name) {
+    ++correct;
+    document.getElementById("name").innerHTML = "Correct!";
+  } else {
+    document.getElementById("name").innerHTML = "Wrong!";
   }
 }
 
-function stopPractice(){
-  document.getElementById("file").style.opacity="0";
-  document.getElementById("show").style.display="none";
-  document.getElementById("ans").style.display="none";
-  document.getElementById("answ").style.display="none";
+function stopPractice() {
+  document.getElementById("file").style.opacity = "0";
+  document.getElementById("show").style.display = "none";
+  document.getElementById("ans").style.display = "none";
+  document.getElementById("answ").style.display = "none";
   time = document.getElementById("time").value;
-  document.getElementById("name").innerHTML="You got "+correct+"/"+time+"!";
+  document.getElementById("name").innerHTML = "You got " + correct + "/" + time + "!";
 }
 
-function startTest(){
+function startTest() {
 
 }
